dd5b1ae0d52940d86c85cdae9dbafca60d86ea59
yuyang0312 <784795097@qq.com>
2018-06-09 09:28:33 +0800

    C#

---
+++ sw/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_something_Default : System.Web.UI.Page
+{
+
+    public string zzz = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        zzz = "zzz";
+    }
+}
